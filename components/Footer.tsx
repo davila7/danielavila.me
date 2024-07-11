b83dbc1276b1cfe7b23a61c81001ba6e98561c90
Dani <dan.avila7@gmail.com>
2024-07-11 17:56:26 -0400

    link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
         {/* Social Media Icons */}
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map(({id, img, link}) => (
-            <a key={id} href={link} target="_blank">
+            <a key={id} target="_blank">
             <div
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
