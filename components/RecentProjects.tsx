a541ca96b8646c51cb59d2537f4c3d7cdb068932
Dani <dan.avila7@gmail.com>
2024-07-13 20:04:16 -0400

    fix: Update the title of recent projects section to "my recent projects" for clarity.

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -8,7 +8,7 @@ const RecentProjects = () => {
     <div className='py-20' id="projects">
       <h1 className='heading'>
         A small selection of {' '}
-        <span className='text-purple'>resent projects</span>
+        <span className='text-purple'>my recent projects</span>
       </h1>
       <div className='flex flex-wrap items-center justify-center p-10 gap-x-24 gap-y-8 mt-10'>
         {projects.map(({ id, title, des, img, iconLists, link}) => (
